19d9d513986cdd5518c90edb88eae6e3e13c228d
Andrea <righettiandrea@outlook.com>
2020-03-24 12:57:45 +0100

    First test working (been able to add an activitybp added an entry and printed it)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <ctime>
 #include "ActivityBluePrint.h"
+#include "Colors.h"
 
 int main() {
-    ActivityBluePrint math("math",)
+    Colors c(0, 0, 0);
+    ActivityBluePrint math("math", c, "productivity", "");
+    tm a;
+    a.tm_hour = 10;
+    a.tm_min = 00;
+
+    tm b;
+    b.tm_hour = 11;
+    b.tm_min = 00;
+    math.addEntry(a, b);
+
+    math.printVector();
 }
